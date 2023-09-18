1188e47eab3b662b82f2ffa1f7aed5edcbf5e768
KingJuu <136377830+KingJuu@users.noreply.github.com>
2023-09-17 21:37:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,38 +6,73 @@ document.querySelector('#btn-enviar').addEventListener('click', function(){
 
         if(input.id == 'x'){
             var x = parseFloat(input.value);
-            console.log(x)
+
         }else if(input.id == 'y'){
             var y = parseFloat(input.value);
-            console.log(y)
+            
         }else if(input.id == 'r'){
             var r = parseFloat(input.value);
-            console.log(r)
+            
         }else if(input.id == 'L'){
             var L = parseFloat(input.value);
-            console.log(L)
+            
         }else if(input.id == 'h'){
             var h = parseFloat(input.value);
-            console.log(h)
+            
         }
     }
 
-    //m and n
+
+    //altura ao quadrado
+    var heightS = h**2;
+
+    //m 
     var radM1 = (y-r)**2;
-    var radN1 = (x-r)**2;
-    var rad2 = h**2;
-
-    var radicandoTM = radM1 + rad2;
-    var radicandoTN = radN1 + rad2;
-
+    var radicandoTM = radM1 + heightS;
     var m = Math.sqrt(radicandoTM);
+
+    //n
+    var radN1 = (x-r)**2;
+    var radicandoTN = radN1 + heightS;
     var n = Math.sqrt(radicandoTN);
 
+    //A and B
+    const sin = [0, 0.5, 0.866025404, 1];
+    const cos = [1, 0.866025404, 0.5, 0];
 
+    var testeB = [];
+    var testeA = [];
+
+    for(var i = 0; i<sin.length; i++){
+
+        let calc1 = (L - sin[i]*r)**2;
+        
+        //A
+        let calcA2 = (x - cos[i]*r)**2 + heightS;
+        let calcA3 = calc1 + calcA2;
+        var calcA4 = Math.sqrt(calcA3);
+        testeA += `A${i+1}) ${calcA4}</p>`;
+
+        //B
+        let calcB2 = (y - cos[i]*r)**2 + heightS;
+        let calcB3 = calc1 + calcB2;
+        var calcB4 = Math.sqrt(calcB3);
+        testeB += `B${i+1}) ${calcB4}</p>`;
+
+        
+    }
+    
+    //exibir resultado
     const result = document.querySelector('.resultado');
-    result.innerHTML = `<h1>Resultados</h1>
-                        <p>m) ${m}</p>
-                        <p>n) ${n}</p>`
+    result.innerHTML = `
+    <h1>Resultados</h1>
+        <p>m) ${m}</p>
+        <p>n) ${n}</p>
+        <p>${testeA}
+        <p>${testeB}
+        `
+        ;
+    
 }
 )
 
